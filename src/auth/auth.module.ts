af8b633c79ed56f571346f22f9c5afcbbcbabcfc
filland <kubalexei@gmail.com>
2021-07-02 00:22:16 +0300

    replace hardcoded jwt secret to a env variable that is read from env file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,22 @@ import { UserRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        // expires in 3600 seconds = 1 hour
-        expiresIn: 3600
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            // expires in 3600 seconds = 1 hour
+            expiresIn: 3600
+          }
+        }
       }
     }),
     TypeOrmModule.forFeature([UserRepository])],
