e779a3b48c8e4b64958a30a3e0f905876c7fee5d
filland <kubalexei@gmail.com>
2021-07-02 00:06:31 +0300

    get rid of hardcoded configuration for database and use env variables from a file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,33 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.stage.${process.env.STAGE}`
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "task-management",
-      // the following two params seems to need to be set to false if i want to 
-      // create tables using plain sql (meaning do not want to use generation of entities by TypeORM)
-      autoLoadEntities: true,
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // here we use dependency injection to inject configService to get env variables 
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: "postgres",
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          // the following two params seems to need to be set to false if i want to 
+          // create tables using plain sql (meaning do not want to use generation of entities by TypeORM)
+          autoLoadEntities: true,
+          synchronize: true
+        };
+      }
     }),
     AuthModule
   ],
